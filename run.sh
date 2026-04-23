b9e6c59e21957cb14e38b51e59127f3024f371cc
alexsanch3zx <asanche2@trincoll.edu>
2026-04-22 21:53:37 -0400

    Cleaned .md and removed .sh files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#env.sh is not pushed to the repo (.gitignore)
-source env.sh
-bash setup.sh
-
-echo " Launching app..."
-python3 -m streamlit run app.py --server.port 8501 --server.address 0.0.0.0
-#then run "bash run.sh"
